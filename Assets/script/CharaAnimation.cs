e14c7725e420c07db1ff5f4fda03361ab135530a
shion_cat <shionairou@gmail.com>
2017-03-21 16:30:59 +0900

    修正

--- Assets/script/CharaAnimation.cs
+++ Assets/script/CharaAnimation.cs
@@ -7,58 +7,52 @@ public class CharaAnimation : MonoBehaviour
 	Vector3 prePosition;
 	bool isDown = false;
 	bool attacked = false;
-	
+
 	public bool IsAttacked()
 	{
 		return attacked;
 	}
-	
+
 	void StartAttackHit()
 	{
 		Debug.Log ("StartAttackHit");
 	}
-	
+
 	void EndAttackHit()
 	{
 		Debug.Log ("EndAttackHit");
 	}
-	
+
 	void EndAttack()
 	{
 		attacked = true;
 	}
-	
+
 	void Start ()
 	{
 		animator = GetComponent<Animator>();
 		status = GetComponent<CharacterStatus>();
-		
+
 		prePosition = transform.position;
 	}
-	
+
 	void Update ()
 	{
-		
 		Vector3 delta_position = transform.position - prePosition;
 		animator.SetFloat("Speed", delta_position.magnitude / Time.deltaTime);
-		
+
 		if(attacked && !status.attacking)
 		{
 			attacked = false;
 		}
 		animator.SetBool("Attacking", (!attacked && status.attacking));
-		
+
 		if(!isDown && status.died)
 		{
 			isDown = true;
 			animator.SetTrigger("Down");
 		}
-		animator.ResetTrigger("Restate");
-		if (Input.GetKeyDown(KeyCode.A)) {
-			isDown = false;
-			animator.SetTrigger("Restate");
-		}
-		
+
 		prePosition = transform.position;
 	}
 }
--- Assets/script/DropItem.cs
+++ Assets/script/DropItem.cs
@@ -8,13 +8,12 @@ public class DropItem : MonoBehaviour {
 		Heal,
 	};
 	public ItemKind kind;
-	
+
 	void OnTriggerEnter(Collider other)
 	{	
 		// Playerか判定
 		if( other.tag == "Player" ){
 			// アイテム取得
-			Debug.Log ("attateruyo");
 			CharacterStatus aStatus = other.GetComponent<CharacterStatus>();
 			aStatus.GetItem(kind);
 			// 取得したらアイテムを消す
@@ -23,13 +22,13 @@ public class DropItem : MonoBehaviour {
 	}
 
 	// Use this for initialization
-	//void Start () {
-//		Vector3 velocity = Random.insideUnitSphere * 2.0f + Vector3.up * 8.0f;
-	//	rigidbody.velocity = velocity;
-	//}
-	
+	void Start () {
+		Vector3 velocity = Random.insideUnitSphere * 2.0f + Vector3.up * 8.0f;
+		GetComponent<Rigidbody>().velocity = velocity;
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
